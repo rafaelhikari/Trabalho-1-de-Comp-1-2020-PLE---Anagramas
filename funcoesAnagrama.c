63974deed3f923aedc5358f9fb948699d2d789f8
Luluquinhas <lucasgocm@dcc.ufrj.br>
2020-11-06 01:27:10 -0300

    Update funcoesAnagrama.c
    adicionado verificação de sistema e looping da mesma palavra.

--- funcoesAnagrama.c
+++ funcoesAnagrama.c
@@ -2,23 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define TAM_MAX 7
 #define DIC_MAX 47
 
 int gPontuacao;
+int sistema;
 
 int puxa_palavra(char *palavra, FILE *dicionario);
 
 int randomiza_palavra_nova(char *palavra, int i, int *linha_atual, FILE *dicionario);
 
-int validar_resposta(char resposta[TAM_MAX], int linha_dicionario, FILE *anagramas);
+int validar_resposta(char *palavra, char resposta[TAM_MAX], int linha_dicionario, FILE *anagramas);
 
 int main(){
     char *palavra = malloc(TAM_MAX * sizeof(char));
     char resposta[TAM_MAX];
-    int i, linha[DIC_MAX];
+    int i, status_resposta, linha[DIC_MAX];
+    bool vitoria = false;
     FILE *ptrFile;
+
+    #ifdef _WIN32
+        printf("Windows\n");
+        sistema = 0;
+    #elif WIN64
+        printf("Windows\n");
+        sistema = 0;
+    #elif linux__
+        printf("Linux\n");
+        sistema = 1;
+    #else
+        printf("Unidentified OS\n");
+    #endif
 
     for(i=0; i<DIC_MAX; i++){
         if((ptrFile = fopen("dicionario.txt", "r")) == NULL){
@@ -30,9 +46,6 @@ int main(){
 
         randomiza_palavra_nova(palavra, i, &linha, ptrFile);
 
-        printf("%d\n", linha[i]);
-        printf("%s\n", palavra);
-
         fclose(ptrFile);
 
         if((ptrFile = fopen("anagramas.txt", "r")) == NULL){
@@ -40,19 +53,33 @@ int main(){
             exit(1);
         }
 
-        printf("Insira uma resposta: ");
-        scanf("%s", &resposta);
+        while(1){
 
-        if((validar_resposta(resposta, linha[i], ptrFile)) == 0){
-            gPontuacao += strlen(resposta);
-            printf("Parabens!\n");
+            if(vitoria == true)printf("Entre 'P' para jogar com a proxima palavra.\n");
+            printf("Pontos: %d\n", gPontuacao);
+            printf("%s\n", palavra);
+
+            printf("Insira uma resposta: ");
+            scanf("%s", &resposta);
+
+            if(resposta == 'P') break;
+
+            status_resposta = validar_resposta(palavra, resposta, linha[i], ptrFile);
+
+            if(status_resposta == 0){
+                gPontuacao += strlen(resposta);
+                printf("Acertou!\n");
+            }
+            else if(status_resposta == 1){
+                printf("Errou!\n");
+            }
+            else if(status_resposta == 2){
+                gPontuacao += 10;
+                printf("Parabens, acertou a principal!\n");
+                vitoria = true;
+            }
         }
-        else{
-            printf("Errou!\n");
-        }
-/*
-        printf("%d\n", gPontuacao);
-*/
+
         fclose(ptrFile);
     }
     return 0;
@@ -95,13 +122,13 @@ int randomiza_palavra_nova(char *palavra, int i, int *linha, FILE *dicionario){
     }
 }
 
-int validar_resposta(char resposta[TAM_MAX], int linha_dicionario, FILE *anagramas){
-    char palavra[TAM_MAX], c;
+int validar_resposta(char *palavra, char resposta[TAM_MAX], int linha_dicionario, FILE *anagramas){
+    char anagrama_atual[TAM_MAX], c;
     int i;
 
     rewind(anagramas);
 
-    for(i=0; i < linha_dicionario; i++){
+    for(i=0; i<linha_dicionario; i++){
         c = fgetc(anagramas);
         while(c != '\n'){
             c = fgetc(anagramas);
@@ -110,8 +137,9 @@ int validar_resposta(char resposta[TAM_MAX], int linha_dicionario, FILE *anagram
 
     c = fgetc(anagramas);
     while(c != '\n'){
-        fscanf(anagramas, "%s", &palavra);
-        if(strcmp(palavra, resposta) == 0) return 0;
+        fscanf(anagramas, "%s", &anagrama_atual);
+        if(strcmp(palavra, resposta) == 0) return 2;
+        if(strcmp(anagrama_atual, resposta) == 0) return 0;
         c = fgetc(anagramas);
     }
     return 1;
